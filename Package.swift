79026243716f3081457c88af2a33be80a3ddf6a9
Arriana Blais <arriblais@gmail.com>
2025-05-05 11:37:51 -0400

    chore: bump otel dependency to latest version (#74)
    <!--
    Thank you for contributing to the project! 💜
    Please see our [OSS process
    document](https://github.com/honeycombio/home/blob/main/honeycomb-oss-lifecycle-and-practices.md#)
    to get an idea of how we operate.
    -->
    
    ## Which problem is this PR solving?
    
    - Closes #<enter issue here>
    
    ## Short description of the changes
    
    ## How to verify that this has the expected result
    
    ---
    
    - [ ] CHANGELOG is updated
    - [ ] README is updated with documentation

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "Honeycomb", type: .static, targets: ["Honeycomb"])
     ],
     dependencies: [
-        .package(url: "https://github.com/open-telemetry/opentelemetry-swift.git", from: "1.14.0")
+        .package(url: "https://github.com/open-telemetry/opentelemetry-swift.git", from: "1.15.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
